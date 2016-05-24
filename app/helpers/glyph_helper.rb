2b162b20d15d4c6ac75e0b2ccbcd72e46b2ac39d
Katharina Peter <gaz@gmx.net>
2016-05-23 17:15:10 -0700

    font-awesome added

---
+++ app/helpers/glyph_helper.rb
@@ -0,0 +1,7 @@
+module GlyphHelper
+  def glyph(name)
+    name = name.to_s.tr('_','-')
+    # <i class="fa fa-camera-retro" aria-hidden="true"></i>
+    content_tag :i, nil, class: "fa fa-#{name}", 'aria-hidden' => true
+  end
+end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,11 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components', 'font-awesome', 'fonts')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile << Proc.new do |path|
+  path =~ /font-awesome\/fonts/ and File.extname(path).in?(['.otf', '.eot', '.svg', '.ttf', '.woff'])
+end
