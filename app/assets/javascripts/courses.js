406604447d5e9ebe29122bc686a57aaa725f2100
Katharina Peter <gaz@gmx.net>
2016-05-30 12:56:36 -0700

    calendar - hide scrollbar and adjust size automatically

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -2,4 +2,6 @@ $(document).ready(function() {
   $('#courses-calendar').fullCalendar({
     events: $('#courses-calendar').data('event-url')
   })
+  $('#courses-calendar').fullCalendar('option', 'height', 'auto');
+  $('#courses-calendar').fullCalendar('option', 'contentHeight', 'auto');
 });
