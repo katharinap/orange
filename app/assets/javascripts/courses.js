f7d68d2043baae788252eb9cfc911dca5234b5cd
Katharina Peter <gaz@gmx.net>
2016-06-05 14:23:11 -0700

    courses calendar - changed first day of the week to monday

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('#courses-calendar').fullCalendar({
+    firstDay: 1,
     events: $('#courses-calendar').data('event-url'),
     eventClick: function(calEvent, jsEvent, view) {
       $.ajax({ url: calEvent.edit_url })
