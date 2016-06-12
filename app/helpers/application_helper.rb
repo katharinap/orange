68cc68d6b2529416302e0693765f080b4fb90e22
Katharina Peter <gaz@gmx.net>
2016-06-12 14:35:51 -0700

    collapse navbar on small devices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 module ApplicationHelper
+  def main_menu_links
+    [
+      [exercises_path, 'Exercises', 'bar-chart'],
+      [courses_path, 'Classes', 'calendar'],
+      [weights_path, 'Weight', 'line-chart'],
+      # rubocop:disable Metrics/LineLength
+      [user_push_up_challenge_entries_path(current_user), '100 Push-Ups', 'list-ul']
+      # rubocop:enable Metrics/LineLength
+    ]
+  end
+
   def sigma
     '&Sigma;'.html_safe
   end
--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -4,4 +4,10 @@ module NavHelper
     base_class << ' active' if current_page?(path)
     base_class
   end
+
+  def dropdown_nav_link_classes(path)
+    base_class = 'dropdown-item'
+    base_class << ' active' if current_page?(path)
+    base_class
+  end
 end
