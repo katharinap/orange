ff0d7d2309ca4f161716f3d7485719ffb456eff1
Katharina Peter <gaz@gmx.net>
2016-06-14 21:59:49 -0700

    fixed date for next push up challenge entry

--- app/controllers/push_up_challenge_entries_controller.rb
+++ app/controllers/push_up_challenge_entries_controller.rb
@@ -4,6 +4,7 @@ class PushUpChallengeEntriesController < ApplicationController
     entries = @user.push_up_challenge_entries.order(:week, :day)
     @entries_done, entries_to_do = entries.partition(&:done_at)
     @editable_entry = entries_to_do.first
+    @today = today
     @entries_to_do = entries_to_do.drop(1)
   end
 
